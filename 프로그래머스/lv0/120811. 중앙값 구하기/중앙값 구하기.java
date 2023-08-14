efd50c07126ae998081ebebda0b8faabfc1e58b3
youngungpark <141301678+youngungpark@users.noreply.github.com>
2023-08-14 17:05:40 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for(int i=0; i<array.length; i++){
+            for(int j=0; j<array.length; j++){
+                int temp = 0;
+                if(array[i] > array[j]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array[array.length/2];
+    }
+}
