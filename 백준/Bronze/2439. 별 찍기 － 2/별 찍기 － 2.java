f558a17758f95a8aa7ae5f1fdffab7f8794b5d83
youngungpark <141301678+youngungpark@users.noreply.github.com>
2023-08-28 15:19:41 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 268 ms, Memory: 16852 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        int count = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=count; i++){
+            for(int j=count; j>i; j--){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
