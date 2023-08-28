a572d4bd54e721281d8c1ef55573d2e8e0d2bff4
youngungpark <141301678+youngungpark@users.noreply.github.com>
2023-08-28 14:09:12 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,20 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+////        int y = Integer.parseInt(br.readLine());
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
