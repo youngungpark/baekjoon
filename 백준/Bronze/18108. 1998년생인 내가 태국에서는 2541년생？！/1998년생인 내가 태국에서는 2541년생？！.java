19be80a7e1e573c1f167bd1965cabfe96dce1fd3
youngungpark <141301678+youngungpark@users.noreply.github.com>
2023-08-28 13:52:46 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 132 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,16 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int y = Integer.parseInt(br.readLine());
+//        int b = Integer.parseInt(st.nextToken());
+        System.out.print(y-543);
+
+    }
+}
