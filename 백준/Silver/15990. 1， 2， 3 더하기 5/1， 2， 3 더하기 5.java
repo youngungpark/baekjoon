8f1340516601214f74d53949025067aef2922cbd
youngungpark <141301678+youngungpark@users.noreply.github.com>
2023-09-06 18:24:14 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 196 ms, Memory: 21360 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
@@ -1,32 +1,50 @@
+import java.io.*;
+import java.sql.Array;
+import java.util.*;
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// 오버플로우 예외가 발생할 수 있으므로 long 자료형 사용
-		long result;
-		// [n][사용할 수]
-		long[][] dp = new long[100001][4];
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// 경우의 수가 1개 이므로 1로 초기화
-		dp[1][1] = dp[2][2] = dp[3][3] = 1;
-		
-		for(int i=1; i<dp.length; i++) {
-			if(i>1) dp[i][1] = (dp[i-1][2] + dp[i-1][3]) % 1000000009;
-			if(i>2) dp[i][2] = (dp[i-2][1] + dp[i-2][3]) % 1000000009;
-			if(i>3) dp[i][3] = (dp[i-3][1] + dp[i-3][2]) % 1000000009;
-		}
-		
-		int count = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<count; i++) {
-			int num = Integer.parseInt(br.readLine());
-			result = (dp[num][1] + dp[num][2] + dp[num][3]) % 1000000009;
-			System.out.println(result);
-		}
-	}
-}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+        long[][] dp = new long[100001][4];
+        dp[1][1] = 1;
+        dp[2][2] = 1;
+        dp[3][3] = 1;
+        dp[3][1] = 1; // 2+1
+        dp[3][2] = 1; // 1+2
+
+        for(int j=4; j<100001; j++){
+            dp[j][1] = (dp[j-1][2] + dp[j-1][3]) %1_000_000_009;
+            dp[j][2] = (dp[j-2][1] + dp[j-2][3]) %1_000_000_009;
+            dp[j][3] = (dp[j-3][1] + dp[j-3][2]) %1_000_000_009;
+        }
+        
+        for(int i=0; i<count; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            
+            bw.write((dp[num][1]+dp[num][2]+dp[num][3])%1_000_000_009+"\n");
+        }
+
+
+
+        bw.flush();
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+};
