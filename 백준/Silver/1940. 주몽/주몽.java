68e72ba7409f7f9a23005f94c953499b02579c2d
youngungpark <141301678+youngungpark@users.noreply.github.com>
2023-09-16 21:12:31 +0900

    [Silver IV] Title: 주몽, Time: 200 ms, Memory: 16424 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,45 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int n = Integer.parseInt(br.readLine());
+        int target = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] iron = new int[n];
+
+        for(int i=0; i<n; i++){
+            iron[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(iron);
+
+        int count =0;
+        int i=0;
+        int j = n-1;
+
+        while(i<j){
+            if(iron[i]+iron[j] < target){
+                i++;
+            }else if(iron[i]+iron[j] > target){
+                j--;
+            }else{
+                count++;
+                i++;
+                j--;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
