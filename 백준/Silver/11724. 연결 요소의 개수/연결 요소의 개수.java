590d529809434c9685ce25ac6bd235c49cb25d50
youngungpark <141301678+youngungpark@users.noreply.github.com>
2023-09-23 17:55:16 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 708 ms, Memory: 141448 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,53 +1,53 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
-/**
- * Main
- */
 public class Main {
     static boolean[] visited;
-    static boolean[][] gp;
-    static int n;
-    static int m;
+    static ArrayList<Integer>[] A;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         visited = new boolean[n + 1];
-        gp = new boolean[n + 1][n + 1];
+        A = new ArrayList[n+1];
+        for (int i = 1; i < n + 1; i++) {
+            A[i] = new ArrayList<Integer>();
+        }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            gp[s][e] = true;
-            gp[e][s] = true;
+            A[s].add(e);
+            A[e].add(s);
         }
         int count = 0;
         for (int i = 1; i < n + 1; i++) {
             if (!visited[i]) {
                 count++;
-                dfs(i);
+                DFS(i);
             }
         }
         System.out.println(count);
-
     }
 
-    static void dfs(int v) {
+    static void DFS(int v) {
         if (visited[v]) {
             return;
         }
         visited[v] = true;
-        for (int i = 1; i < n + 1; i++) {
-            if (!visited[i] && gp[v][i]) {
-                dfs(i);
+        for (int i : A[v]) {
+            if (visited[i] == false) {
+                DFS(i);
             }
+
         }
     }
+
 }
