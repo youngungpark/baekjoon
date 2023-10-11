19d7c56a479c3277d480ce80547070800e47239f
youngungpark <141301678+youngungpark@users.noreply.github.com>
2023-10-11 20:21:30 +0900

    [Bronze III] Title: 전자레인지, Time: 144 ms, Memory: 15868 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.java
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -1,34 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
 
-/**
- * Main
- */
 public class Main {
-
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+
         int[] countArr = new int[3];
         boolean isTrue = true;
-        while (N > 0) {
-            if (N % 300 == 0) {
-                countArr[0] = N / 300;
-                break;
-            } else if (N % 60 == 0) {
-                countArr[1] = N / 60;
-                break;
-            } else if (N >= 10) {
-                N -= 10;
-                countArr[2]++;
-            } else {
-                isTrue = false;
-                break;
-            }
+
+        if (N % 10 != 0) {
+            isTrue = false;
+        } else {
+            countArr[0] = N / 300;
+            N %= 300;
+
+            countArr[1] = N / 60;
+            N %= 60;
+
+            countArr[2] = N / 10;
         }
+
         if (isTrue) {
             for (int i : countArr) {
                 System.out.print(i + " ");
